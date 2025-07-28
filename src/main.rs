922ec83f1f6bdd80ff2ad63337316513a2e5a807
Martin Toman <25009432+tinybeachthor@users.noreply.github.com>
2025-07-27 22:02:25 -0500

    Setup yew component SSR

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use yew::prelude::*;
+use yew::ServerRenderer;
+
+#[function_component]
+fn App() -> Html {
+    html! {<div>{"Hello, World!"}</div>}
+}
+
+#[tokio::main]
+async fn main() {
+    let renderer = ServerRenderer::<App>::new();
+
+    let rendered = renderer.render().await;
+
+    println!("{}", rendered);
 }
