75773dbb287e54c8498284d72877a437af6fdcc6
Martin Toman <25009432+tinybeachthor@users.noreply.github.com>
2025-08-03 14:14:52 -0500

    add component Img

---
+++ src/components/img.rs
@@ -0,0 +1,28 @@
+pub use yew::{function_component, html, AttrValue, Properties};
+
+#[derive(Properties, PartialEq)]
+pub struct Props {
+    pub src: AttrValue,
+
+    #[prop_or(AttrValue::Static("32"))]
+    pub width: AttrValue,
+    #[prop_or(AttrValue::Static("32"))]
+    pub height: AttrValue,
+
+    pub alt: AttrValue,
+}
+
+#[function_component]
+pub fn Img(props: &Props) -> yew::Html {
+    let Props { src, width, height, alt } = props;
+
+    yew::html! {
+        <img
+            src={src}
+            width={width}
+            height={height}
+            alt={alt}
+            style="display:'block'; outline:'none'; border:'none'; textDecoration: 'none'"
+        />
+    }
+}
--- src/components/mod.rs
+++ src/components/mod.rs
@@ -5,9 +5,11 @@ pub mod email_html;
 pub mod container;
 pub mod text;
 pub mod link;
+pub mod img;
 
 pub use body::Body;
 pub use email_html::EmailHtml;
 pub use container::Container;
 pub use text::Text;
 pub use link::Link;
+pub use img::Img;
