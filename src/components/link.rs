c1443cbaa002e4e8c9fefab9590a235d2269e1b3
Martin Toman <25009432+tinybeachthor@users.noreply.github.com>
2025-08-03 14:06:40 -0500

    add component Link

---
+++ src/components/link.rs
@@ -0,0 +1,23 @@
+use yew::{function_component, html, AttrValue, Html, Properties};
+
+#[derive(Properties, PartialEq)]
+pub struct Props {
+    #[prop_or(AttrValue::Static("_blank"))]
+    pub target: AttrValue,
+
+    pub children: Html,
+}
+
+#[function_component]
+pub fn Link(props: &Props) -> Html {
+    let Props { target, children } = props;
+
+    html! {
+        <a
+            style="color:'#067df7'; textDecorationLine:'none'"
+            target={target}
+        >
+            {children.clone()}
+        </a>
+    }
+}
--- src/components/mod.rs
+++ src/components/mod.rs
@@ -4,8 +4,10 @@ pub mod body;
 pub mod email_html;
 pub mod container;
 pub mod text;
+pub mod link;
 
 pub use body::Body;
 pub use email_html::EmailHtml;
 pub use container::Container;
 pub use text::Text;
+pub use link::Link;
