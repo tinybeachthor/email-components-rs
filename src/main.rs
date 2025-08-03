708844efa7ef2ae891b9de17cb0d4745f31c5aa2
Martin Toman <25009432+tinybeachthor@users.noreply.github.com>
2025-08-03 13:35:44 -0500

    bring in the rest of the GitHub email template
    https://demo.react.email/preview/notifications/github-access-token?view=source&width=600&height=797&lang=jsx

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,56 @@
 use yew::ServerRenderer;
 use yew::prelude::*;
 
-use email_components_rs::components::{body::Body, email_html::EmailHtml};
+use email_components_rs::components::{Body, EmailHtml, Container};
 
 const DOCTYPE: &str = r#"<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">"#;
 
 #[function_component]
 fn App() -> Html {
+    // TODO
+    let base_url = "";
+    let username = "USERNAME";
+
     html! {
         <EmailHtml>
-            <Body>
-                <div>
-                    {"Hello, World!"}
-                </div>
-            </Body>
+        <Body>
+        <Container>
+            <Img
+              src={format!("{base_url}/static/github.png")}
+              width="32"
+              height="32"
+              alt="Github"
+            />
+
+            <Text>
+              <strong>{'@'}{username}</strong>
+              {", a personal access was created on your account."}
+            </Text>
+
+            <Section>
+              <Text>
+                {"Hey "}<strong>{username}</strong>{'!'}
+              </Text>
+              <Text>
+                {"A fine-grained personal access token ("}
+                <Link>{"resend"}</Link>
+                {") was recently added to your account."}
+              </Text>
+
+              <Button>{"View your token"}</Button>
+            </Section>
+
+            <Text>
+              <Link>{"Your security audit log"}</Link>
+              {" - "}
+              <Link>{"Contact support"}</Link>
+            </Text>
+
+            <Text>
+              {"GitHub, Inc. - 88 Colin P Kelly Jr Street - San Francisco, CA 94107"}
+            </Text>
+        </Container>
+        </Body>
         </EmailHtml>
     }
 }
