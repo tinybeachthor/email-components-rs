ddca569c0e95109ac6dae411297a99b335902e57
Martin Toman <25009432+tinybeachthor@users.noreply.github.com>
2025-08-06 23:09:27 -0500

    example/serve respond with html
    Closes #8

--- email-examples/examples/serve.rs
+++ email-examples/examples/serve.rs
@@ -1,4 +1,4 @@
-use axum::{Router, routing::get};
+use axum::{Router, response::Response, routing::get};
 use yew::ServerRenderer;
 
 use email_examples::email::github::Email;
@@ -7,12 +7,18 @@ use email_examples::email::github::Email;
 async fn main() {
     let app = Router::new().route("/", get(render));
 
-    // run with hyper, listening globally on port 3000
+    // run with hyper, listening globally
+    println!("listening on http://0.0.0.0:3000");
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn render() -> String {
+async fn render() -> Response {
     let renderer = ServerRenderer::<Email>::new();
-    renderer.render().await
+    let output = renderer.render().await;
+
+    Response::builder()
+        .header("content-type", "text/html")
+        .body(output.into())
+        .expect("valid response")
 }
